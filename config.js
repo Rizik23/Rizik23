8fd79f700268a5c8bc09bd5086cece7ff51aea6a
Always Dragon <opsionalrizik@gmail.com>
2026-02-22 18:10:09 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 module.exports = {
 
  // ==== Bot Setting ========
-  botToken: "8542010252:AAGiMXol_TwOmdbWGP52KPXQABas4jZGkf4",
+  botToken: "8542010252:AAGiMXol_TwOmdbWGP52KPXQABas4jZGkf4,
   botUsername: "ElikaMd_bot",
-  menuImage: "http://tmpfiles.org/dl/24259196/1771122571414.jpg",
+  menuImage: "https://tmpfiles.org/dl/25539099/1771702158731.png",
+  katalogImage: "https://tmpfiles.org/dl/25538833/file_000000003d2472089efb8382ea472f0c.png",
   prefix: "/",
   ownerName: "kaell",
   ownerUsername: "Myzxa",
   ownerId: "1402991119",
   channelId: "-1003722756127",
-  channelLink: "https://t.me/Mythe5",
+  channelLink: "https://t.me/MyThe5",
   
   // ===== Userbot Setting =====
   apiId: 32424796, // api_id Telegram
@@ -22,10 +23,10 @@ module.exports = {
 
   // ===== OrderKuota Config =====
   orderkuota: {
-    apikey: "verlangid",
-    username: "mrwibuss",
-    token: "931165:o5EVQ9YOwpJ4jmLltM0z8scyXURSBbZu",
-    qrisCode: "00020101021126670016COM.NOBUBANK.WWW01189360050300000879140214135152866177810303UMI51440014ID.CO.QRIS.WWW0215ID20232438849050303UMI5204511153033605802ID5908MRWIBUSS6013JAKARTA TIMUR61051311062070703A0163043C15"
+    apikey: "",
+    username: "",
+    token: "",
+    qrisCode: ""
   },
 
   // ===== Pakasir Config =====
@@ -49,9 +50,9 @@ module.exports = {
   egg: "15",
   nestid: "5",
   loc: "1",
-  domain: "https://rubika.yazxxpanel.my.id",
-  apikey: "ptlc_kJjXsd8p2DucAJUmSoC4X2lhIgs0MDfcX3G7ROHFLia",
-  capikey: "ptla_KiZZnscwwjG4O68vNVrkGLK2AwEUiEM3yWSIjM6v0XB", 
+  domain: "",
+  apikey: "",
+  capikey: "", 
   
   // ===== Setting Api Subdomain ===
   subdomain: {
